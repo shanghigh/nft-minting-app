cbd13d974bcf89d7f73ee8d29eb6dbdf73c732f3
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-02-27 00:57:22 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mint(blockchain.account, mintAmount)
+      .batchMint(blockchain.account, mintAmount)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 25) {
+      newMintAmount = 25;
     }
     setMintAmount(newMintAmount);
   };
